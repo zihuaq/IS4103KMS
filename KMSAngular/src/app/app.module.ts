b8deea1039fea6dac01dfa2a00f048f539a51e2b
Zep Low <zeplherc@comp.nus.edu.sg>
2020-09-04 23:21:37 +0800

    second commit

--- KMSAngular/src/app/app.module.ts
+++ KMSAngular/src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { CreateNewUserComponent } from './create-new-user/create-new-user.component';
+import { IndexComponent } from './index/index.component';
+import { UserLoginPageComponent } from './user-login-page/user-login-page.component';
+import { ViewAllUsersComponent } from './view-all-users/view-all-users.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: IndexComponent},
+  { path: 'signup', component: CreateNewUserComponent},
+  { path: 'login', component: UserLoginPageComponent} 
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    CreateNewUserComponent,
+    IndexComponent,
+    UserLoginPageComponent,
+    ViewAllUsersComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
