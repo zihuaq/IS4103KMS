47456ca516658d7a2e7d8bb482835e2c4261ef5a
jeremy <j3r3my1996@gmail.com>
2020-09-07 23:41:53 +0800

    fix imports for user.ts

--- KMSAngular/src/app/profile/overview/overview.component.ts
+++ KMSAngular/src/app/profile/overview/overview.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/classes/user';
+import { User } from '../../classes/user';
 
 @Component({
   selector: 'app-overview',
