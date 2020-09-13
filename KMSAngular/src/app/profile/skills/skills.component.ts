9074bb7259e083329326823c7a79fa5df856c8b2
jeremy <j3r3my1996@gmail.com>
2020-09-14 00:42:53 +0800

    fix select2 component

--- KMSAngular/src/app/profile/skills/skills.component.ts
+++ KMSAngular/src/app/profile/skills/skills.component.ts
@@ -1,22 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Tag } from '../../classes/tag';
-import { TagService } from '../../tag.service'
+import { TagService } from '../../tag.service';
 import { User } from '../../classes/user';
+
+declare var $: any;
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent implements OnInit {
   @Input() user: User;
-  skillTags: Tag[]
-  constructor(private tagService: TagService) { }
+  skillTags: Tag[];
+
+  constructor(private tagService: TagService) {}
 
   ngOnInit(): void {
-    this.tagService.getAllSkillTags().subscribe(
-      response => {
-        this.skillTags = response;
-      });
+    this.tagService.getAllSkillTags().subscribe((response) => {
+      this.skillTags = response;
+    });
+    $('.select2').select2();
   }
 }
