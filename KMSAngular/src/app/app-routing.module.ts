1ecdfc5a6a0f0b22e18e6621fef210f467971828
jeremy <j3r3my1996@gmail.com>
2020-09-17 12:47:04 +0800

    revert some stuff

--- KMSAngular/src/app/app-routing.module.ts
+++ KMSAngular/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { ProfileComponent } from './profile/profile.component';
 import { IndexComponent } from './index/index.component';
 import { CreateNewUserComponent } from './create-new-user/create-new-user.component';
 import { UserLoginPageComponent } from './user-login-page/user-login-page.component';
-import { ViewAllUsersComponent } from './view-all-users/view-all-users.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { SearchUsersComponent } from './search-users/search-users.component';
 
@@ -14,7 +13,6 @@ const routes: Routes = [
   { path: 'login', component: UserLoginPageComponent },
   { path: 'profile/:userid', component: ProfileComponent },
   { path: 'profile', component: ProfileComponent },
-  { path: 'viewAllUsers', component: ViewAllUsersComponent },
   { path: 'notifications', component: NotificationsComponent },
   { path: 'searchUsers', component: SearchUsersComponent },
   { path: '', component: IndexComponent },
