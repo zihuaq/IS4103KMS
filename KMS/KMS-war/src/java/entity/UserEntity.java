aab0c82d746bcda596a027f478b20c6e162fbc1f
jeibeisoc <jiebei@comp.nus.edu.sg>
2020-09-18 18:04:46 +0800

    edit project user relationship

--- KMS/KMS-war/src/java/entity/UserEntity.java
+++ KMS/KMS-war/src/java/entity/UserEntity.java
@@ -78,16 +78,20 @@ public class UserEntity implements Serializable {
     private List<ReviewEntity> reviewsReceived;
     @OneToMany(mappedBy = "projectOwner")
     private List<ProjectEntity> projectsOwned;
-    @ManyToMany(mappedBy = "projectMembers")
+    @JoinTable(name = "projectMembers")
+    @ManyToMany
     private List<ProjectEntity> projectsJoined;
-    @ManyToMany(mappedBy = "projectAdmins")
+    @JoinTable(name = "projectAdmins")
+    @ManyToMany
     private List<ProjectEntity> projectAdmins;
     @OneToMany(mappedBy = "postOwner")
     private List<PostEntity> posts;
     @OneToMany(mappedBy = "groupOwner")
     private List<GroupEntity> groupsOwned;
+    @JoinTable(name= "groupMembers")
     @ManyToMany(mappedBy = "groupMembers")
     private List<GroupEntity> groupsJoined;  
+    @JoinTable(name= "groupAdmins")
     @ManyToMany(mappedBy = "groupAdmins")
     private List<GroupEntity> groupAdmins;
     @OneToMany
