a20546239847983d50ae06984c5a32d4fe62b4d4
jeibeisoc <jiebei@comp.nus.edu.sg>
2020-09-24 01:23:49 +0800

    add mrp session bean

---
+++ KMS/KMS-war/src/java/ejb/session/stateless/MaterialResourcePostingSessionBeanLocal.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateless;
+
+import Exception.NoResultException;
+import entity.MaterialResourcePostingEntity;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author chai
+ */
+@Local
+public interface MaterialResourcePostingSessionBeanLocal {
+
+    public Long createMaterialResourcePosting(MaterialResourcePostingEntity newMrp, Long projectId) throws NoResultException;
+
+    public MaterialResourcePostingEntity getMrpById(Long mrpId) throws NoResultException;
+
+    public List<MaterialResourcePostingEntity> getListOfMaterialResourcePostingByProjectId(Long projectId);
+
+    public void updateMaterialResourcePosting(MaterialResourcePostingEntity mrpToUpdate);
+
+    public void deleteMaterialResourcePosting(Long mrpId) throws NoResultException;
+    
+}
