b957c0788134cbcbf3184966ee0892144ad9e82b
jeibeisoc <jiebei@comp.nus.edu.sg>
2020-10-03 01:53:06 +0800

    edit view all project page

--- KMSIonic/src/app/pages/project/view-all-project/view-all-project.page.ts
+++ KMSIonic/src/app/pages/project/view-all-project/view-all-project.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Location } from "@angular/common";
+import { Router } from '@angular/router';
+
+import { Project } from 'src/app/classes/project';
+import { ProjectService } from 'src/app/services/project.service';
+import { IonSearchbar } from "@ionic/angular";
 
 @Component({
   selector: 'app-view-all-project',
@@ -7,9 +13,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAllProjectPage implements OnInit {
 
-  constructor() { }
+  @ViewChild("searchBar") searchBar: IonSearchbar
+  projects: Project[];
+  filteredProjects: Project[];
+  preliminarySearchProject: Project[];
+
+  constructor(private location: Location,
+    private projectService: ProjectService,
+    private router: Router) { }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+    this.projectService.getAllProject().subscribe(
+      response => {
+        this.projects = response.projects;
+        this.filteredProjects = this.projects;
+      }
+    );
+  }
+
+  goBack() {
+    this.location.back()
+  }
+
+  setFilteredItems(searchTerm) {
+    console.log(searchTerm)
+
+    if (searchTerm && searchTerm != "") {
+      this.filteredProjects = this.projects.filter(
+        (project) => {
+          return project.name.toLowerCase().includes(searchTerm.toLowerCase())
+        }
+      )
+    } else {
+      this.filteredProjects = this.projects
+    }
+    this.preliminarySearchProject = this.filteredProjects.slice(0, 6)
+  }
+
+  viewProjectDetails(event, project) {
+    this.router.navigate(["project-details/" + project.projectId]);
+  }
+
+  createProject() {
+    this.router.navigate(["create-new-project"]);
   }
 
 }
