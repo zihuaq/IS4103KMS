61afac1f7942e75bd7043648ecdd0ae144eb83e0
jeibeisoc <jiebei@comp.nus.edu.sg>
2020-10-10 23:05:26 +0800

    add view activity tab component

---
+++ KMSAngular/src/app/project/viewProject/activity-tab/activity-tab.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ActivityTabComponent } from './activity-tab.component';
+
+describe('ActivityTabComponent', () => {
+  let component: ActivityTabComponent;
+  let fixture: ComponentFixture<ActivityTabComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ActivityTabComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ActivityTabComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
