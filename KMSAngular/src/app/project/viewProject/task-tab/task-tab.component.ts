5f56a955a4de9eb6357be801f17d2a012e703036
ZiHua <e0335699@u.nus.edu>
2020-10-19 01:19:11 +0800

    add link validation and export gantt chart

--- KMSAngular/src/app/project/viewProject/task-tab/task-tab.component.ts
+++ KMSAngular/src/app/project/viewProject/task-tab/task-tab.component.ts
@@ -3,10 +3,12 @@ import { Task } from '../../../classes/task';
 import { Link } from '../../../classes/link';
 import { LinkService } from '../../../link.service';
 import { TaskService } from '../../../task.service';
-import { gantt } from 'dhtmlx-gantt';
 import { ActivatedRoute, Router } from '@angular/router';
 import { forkJoin } from 'rxjs';
+
 import * as moment from 'moment';
+import { gantt } from 'dhtmlx-gantt';
+import "./api.js";
 
 declare var $: any;
 
@@ -46,6 +48,7 @@ export class TaskTabComponent implements OnInit {
 			}
     )
     
+    //crud of tasks and links
     gantt.attachEvent("onAfterTaskAdd", (id, task) => {
       this.taskService.createNewTask(task, this.projectId).subscribe(
         response => {
@@ -64,7 +67,6 @@ export class TaskTabComponent implements OnInit {
     gantt.attachEvent("onAfterTaskUpdate", (id, task) => {
       this.taskService.updateTask(task).subscribe(
         response => {
-          console.log(task);
           $(document).Toasts('create', {
             class: 'bg-success',
             title: 'Success',
@@ -116,11 +118,132 @@ export class TaskTabComponent implements OnInit {
         });
     }, null);
 
+    //for search tasks
     gantt.attachEvent("onBeforeTaskDisplay", (id) => {
       if (this.hasSubstr(id)) {
         return true;
       } else {
         return false;
+      }
+    }, null);
+
+    //for link validation
+    gantt.attachEvent("onLinkCreated", (link) => {
+      var check = this.checkLink(link, "link", null);
+
+      if (check[0] == false) {
+        $(document).Toasts('create', {
+          class: 'bg-warning',
+          title: 'Warning',
+          autohide: true,
+          delay: 6000,
+          body: check[1],
+        });
+        return false;
+      } else {
+        return true;
+      }
+    }, null);
+
+    gantt.attachEvent("onBeforeTaskChanged", (id, mode, task) => {
+      var sourceLinks = task.$source; 
+      var targetLinks = task.$target;
+      var isValid = true;
+      var body;
+
+      for (let linkId of sourceLinks) {
+        const link = gantt.getLink(linkId);
+        var check = this.checkLink(link, "sourceDrag", task);
+        if (check[0] == false) {
+          body = check[1];
+          isValid = false;
+          break;
+        }
+      }
+
+      if (!isValid) {
+        $(document).Toasts('create', {
+          class: 'bg-warning',
+          title: 'Warning',
+          autohide: true,
+          delay: 6000,
+          body: body,
+        });
+        return false;
+      } else {
+        for (let linkId of targetLinks) {
+          const link = gantt.getLink(linkId);
+          var check = this.checkLink(link, "targetDrag", task);
+          if (check[0] == false) {
+            body = check[1];
+            isValid = false;
+            break;
+          }
+        }
+        if (!isValid) {
+          $(document).Toasts('create', {
+            class: 'bg-warning',
+            title: 'Warning',
+            autohide: true,
+            delay: 6000,
+            body: body,
+          });
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }, null);
+
+    gantt.attachEvent("onLightboxSave", (id, task, is_new) => {
+      if (!is_new) {
+        var sourceLinks = task.$source; 
+        var targetLinks = task.$target;
+        var isValid = true;
+        var body;
+
+        for (let linkId of sourceLinks) {
+          const link = gantt.getLink(linkId);
+          var check = this.checkLink(link, "sourceLightbox", task);
+          if (check[0] == false) {
+            body = check[1];
+            isValid = false;
+            break;
+          }
+        }
+
+        if (!isValid) {
+          $(document).Toasts('create', {
+            class: 'bg-warning',
+            title: 'Warning',
+            autohide: true,
+            delay: 6000,
+            body: body,
+          });
+          return false;
+        } else {
+          for (let linkId of targetLinks) {
+            const link = gantt.getLink(linkId);
+            var check = this.checkLink(link, "targetLightbox", task);
+            if (check[0] == false) {
+              body = check[1];
+              isValid = false;
+              break;
+            }
+          }
+          if (!isValid) {
+            $(document).Toasts('create', {
+              class: 'bg-warning',
+              title: 'Warning',
+              autohide: true,
+              delay: 6000,
+              body: body,
+            });
+            return false;
+          } else {
+            return true;
+          }
+        }
       }
     }, null);
 
@@ -136,15 +259,15 @@ export class TaskTabComponent implements OnInit {
       quick_info: true,
     });
 
-    gantt.templates.quick_info_date = function(start, end, task){
+    gantt.templates.quick_info_date = (start, end, task) => {
       return null;
     };
 
-    gantt.templates.quick_info_title = function(start, end, task){
+    gantt.templates.quick_info_title = (start, end, task) => {
       return "<b>" + task.text + "<b>";
     };
 
-    gantt.templates.quick_info_content = function(start, end, task){ 
+    gantt.templates.quick_info_content = (start, end, task) => { 
       var durationStr: string
         if (task.duration > 1) {
           durationStr = task.duration + " days"
@@ -160,14 +283,16 @@ export class TaskTabComponent implements OnInit {
 
     gantt.templates.task_class = () => {return "task_color"};
 
+    //scaling - zooming in/out
     var hourToStr = gantt.date.date_to_str("%h:%i %A");
     var hourRangeFormat = (step) => {
       return (date) => {
+        date = moment(date).startOf('date').toDate()
+        console.log(moment(date).startOf('date').toDate());
         var intervalEnd = new Date(gantt.date.add(date, step, "hour").valueOf() - 1)
         return hourToStr(date) + " - " + hourToStr(intervalEnd);
       };
     };
-
     var zoomConfig = {
       maxColumnWidth: 72,
       minColumnWidth: 32,
@@ -182,15 +307,15 @@ export class TaskTabComponent implements OnInit {
         },
         {
           name:"hour2",
-          min_column_width:125,
+          min_column_width:28,
           scales:[
             { unit: "day", step: 1, format: "%d %F"},
-            { unit: "hour", step: 12, format: hourRangeFormat(12)}
+            { unit: "hour", step: 1, format: "%G"}
           ]
         },
         {
           name:"day1",
-          min_column_width:80,
+          min_column_width:90,
           scales:[
             {unit: "month", step: 1, format: "%F %Y"},
             {unit: "day", step: 1, format: "%d %M"}
@@ -226,6 +351,7 @@ export class TaskTabComponent implements OnInit {
     gantt.ext.zoom.init(zoomConfig);
     gantt.ext.zoom.setLevel("day1");
 
+    gantt.config.round_dnd_dates = false;
     gantt.config.fit_tasks = true;
     gantt.config.sort = true;
     gantt.config.open_tree_initially = true;
@@ -262,6 +388,90 @@ export class TaskTabComponent implements OnInit {
         return true;
     }
     return false;
+  }
+
+  checkLink(link, type, task) {
+    var source = gantt.getTask(link.source);
+    var target = gantt.getTask(link.target);
+    var isValid;
+    var body;
+
+    if (type == "sourceLightbox") {
+      source = task;
+    } else if (type == "targetLightbox") {
+      target = task;
+    }
+
+    if (link.type == 0 && target.start_date < source.end_date) { //finish_to_start
+      if (type == "link") {
+        body = "Target task cannot start before source task ends";
+      } else if (type == "sourceLightbox" || type == "sourceDrag") {
+        body = "Selected task cannot end after " + target.text + " starts"
+      } else if (type == "targetLightbox" || type == "targetDrag") {
+        body = "Selected task cannot start before " + source.text + " ends"
+      }
+      isValid = false;
+    } else if (link.type == 1 && target.start_date < source.start_date) { //start_to_start
+      if (type == "link") {
+        body = "Target task cannot start before source task starts";
+      } else if (type == "sourceLightbox" || type == "sourceDrag") {
+        body = "Selected task cannot start after " + target.text + " starts"
+      } else if (type == "targetLightbox" || type == "targetDrag") {
+        body = "Selected task cannot start before " + source.text + " starts"
+      }
+      isValid = false;
+    } else if (link.type == 2 && target.end_date < source.end_date) { //finish_to_finish
+      if (type == "link") {
+        body = "Target task cannot end before source task ends";
+      } else if (type == "sourceLightbox" || type == "sourceDrag") {
+        body = "Selected task cannot end after " + target.text + " ends"
+      } else if (type == "targetLightbox" || type == "targetDrag") {
+        body = "Selected task cannot end before " + source.text + " ends"
+      }
+      isValid = false;
+    } else if (link.type == 3 && target.end_date < source.start_date) { //start_to_finish
+      if (type == "link") {
+        body = "Target task cannot end before source task starts";
+      } else if (type == "sourceLightbox" || type == "sourceDrag") {
+        body = "Selected task cannot start after " + target.text + " ends"
+      } else if (type == "targetLightbox" || type == "targetDrag") {
+        body = "Selected task cannot end before " + source.text + " starts"
+      }
+      isValid = false;
+    } else {
+      isValid = true;
+    }
+    return [isValid, body];
+  }
+
+  export(type: string) {
+    switch(type) {
+      case "pdf":
+        gantt.exportToPDF({
+          header:'<style>.task_color {border: 2px solid #7094e2; color: #7094e2; background: #7094e2;}' 
+          + '.task_color .gantt_task_progress {background: #5177ca;}'
+          + '.gantt_task_link .gantt_line_wrapper div {background-color: #5034b8;}'
+          + '.gantt_task_link .gantt_link_arrow {border-left-color: #5034b8; border-right-color: #5034b8; border-bottom-color: #5034b8; border-top-color: #5034b8;}</style>'
+        });
+        break;
+      case "png":
+        gantt.exportToPNG({
+          header:'<style>.task_color {border: 2px solid #7094e2; color: #7094e2; background: #7094e2;}' 
+          + '.task_color .gantt_task_progress {background: #5177ca;}'
+          + '.gantt_task_link .gantt_line_wrapper div {background-color: #5034b8;}'
+          + '.gantt_task_link .gantt_link_arrow {border-left-color: #5034b8; border-right-color: #5034b8; border-bottom-color: #5034b8; border-top-color: #5034b8;}</style>'
+        });
+        break;
+      case "excel":
+        gantt.exportToExcel({});
+        break;
+      case "ical":
+        gantt.exportToICal({});
+        break;
+      case "msproject":
+        gantt.exportToMSProject({});
+        break;
+    }
   }
   
 }
