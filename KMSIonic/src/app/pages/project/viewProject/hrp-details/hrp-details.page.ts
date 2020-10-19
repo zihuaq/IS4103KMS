2ce432d5555c7f98778175c28ff28bba8e695ad6
jeibeisoc <jiebei@comp.nus.edu.sg>
2020-10-20 01:04:02 +0800

    edit hrp page

--- KMSIonic/src/app/pages/project/viewProject/hrp-details/hrp-details.page.ts
+++ KMSIonic/src/app/pages/project/viewProject/hrp-details/hrp-details.page.ts
@@ -43,6 +43,7 @@ export class HrpDetailsPage implements OnInit {
       this.tags = [];
       this.hrp = new HumanResourcePosting();
       this.hrp.appliedUsers = [];
+      this.hrp.tags = [];
      }
 
   ngOnInit() {
