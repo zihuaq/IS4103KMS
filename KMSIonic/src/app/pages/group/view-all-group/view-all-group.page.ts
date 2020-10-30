a414a54ae510313de657e06ba3b3246f189ee5ca
jeremy <j3r3my1996@gmail.com>
2020-10-30 17:42:32 +0800

    add newsfeed to profile and report group on ionic

--- KMSIonic/src/app/pages/group/view-all-group/view-all-group.page.ts
+++ KMSIonic/src/app/pages/group/view-all-group/view-all-group.page.ts
@@ -1,64 +1,63 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Location } from "@angular/common";
+import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
 import { Group } from 'src/app/classes/group';
 import { GroupService } from 'src/app/services/group.service';
-import { IonSearchbar } from "@ionic/angular";
+import { IonSearchbar } from '@ionic/angular';
 
 @Component({
   selector: 'app-view-all-group',
   templateUrl: './view-all-group.page.html',
-  styleUrls: ['./view-all-group.page.scss'],
+  styleUrls: ['./view-all-group.page.scss']
 })
 export class ViewAllGroupPage implements OnInit {
-
-  @ViewChild("searchBar") searchBar: IonSearchbar
+  @ViewChild('searchBar') searchBar: IonSearchbar;
   groups: Group[];
   filteredGroups: Group[];
   preliminarySearchGroup: Group[];
 
-  constructor(private location: Location,
+  constructor(
+    private location: Location,
     private groupService: GroupService,
-    private router: Router) { }
+    private router: Router
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   ionViewWillEnter() {
-    this.groupService.getAllGroup().subscribe(
-      response => {
-        this.groups = response.groups;
-        this.filteredGroups = this.groups;
-      }
-    );
+    this.groupService.getAllGroup().subscribe((response) => {
+      this.groups = response.groups;
+      this.filteredGroups = this.groups;
+    });
   }
 
   goBack() {
-    this.location.back()
+    this.location.back();
   }
 
   setFilteredItems(searchTerm) {
-    console.log(searchTerm)
+    console.log(searchTerm);
 
-    if (searchTerm && searchTerm != "") {
-      this.filteredGroups = this.groups.filter(
-        (group) => {
-          return group.name.toLowerCase().includes(searchTerm.toLowerCase())
-        }
-      )
+    if (searchTerm && searchTerm != '') {
+      this.filteredGroups = this.groups.filter((group) => {
+        return group.name.toLowerCase().includes(searchTerm.toLowerCase());
+      });
     } else {
-      this.filteredGroups = this.groups
+      this.filteredGroups = this.groups;
     }
-    this.preliminarySearchGroup = this.filteredGroups.slice(0, 6)
+    this.preliminarySearchGroup = this.filteredGroups.slice(0, 6);
   }
 
   viewGroupDetails(event, group) {
-    this.router.navigate(["group-details/" + group.groupId]);
+    this.router.navigate(['group-details/' + group.groupId]);
   }
 
   createGroup() {
-    this.router.navigate(["create-new-group"]);
+    this.router.navigate(['create-new-group']);
   }
 
+  sortSDG(sdgList: Tag[]): Tag[] {
+    return sdgList.sort((a, b) => a.tagId - b.tagId);
+  }
 }
--- KMSIonic/src/app/pages/profile/overview/overview.component.ts
+++ KMSIonic/src/app/pages/profile/overview/overview.component.ts
@@ -1,42 +1,38 @@
-import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core"
-import { forkJoin } from "rxjs"
-import { AffiliationRequest } from "src/app/classes/affiliation-request"
-import { FollowRequest } from "src/app/classes/follow-request"
-import { User } from "src/app/classes/user"
-import { AccountPrivacySettingEnum } from "src/app/enum/account-privacy-setting.enum"
-import { UserType } from "src/app/enum/user-type.enum"
-import { UserService } from "src/app/services/user.service"
-import { ActionSheetController, ToastController } from "@ionic/angular"
-import { SocialSharing } from "@ionic-native/social-sharing/ngx"
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { AffiliationRequest } from 'src/app/classes/affiliation-request';
+import { FollowRequest } from 'src/app/classes/follow-request';
+import { User } from 'src/app/classes/user';
+import { AccountPrivacySettingEnum } from 'src/app/enum/account-privacy-setting.enum';
+import { UserType } from 'src/app/enum/user-type.enum';
+import { UserService } from 'src/app/services/user.service';
+import { ToastController } from '@ionic/angular';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
-  selector: "app-overview",
-  templateUrl: "./overview.component.html",
-  styleUrls: ["./overview.component.scss"]
+  selector: 'app-overview',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.scss']
 })
 export class OverviewComponent implements OnInit {
-  @Input() profile: User
-  @Input() loggedInUser: User
-  @Output() profileChanged = new EventEmitter<User>()
-  @Output() userChanged = new EventEmitter<User>()
-  isFollowing: boolean
-  hasSentFollowRequest: boolean
-  isAffiliated: boolean
-  hasSentAffiliationRequest: boolean
-  UserType = UserType
-  profileUrl: string
+  @Input() profile: User;
+  @Input() loggedInUser: User;
+  @Output() init = new EventEmitter();
+  @Input() isFollowing: boolean;
+  @Input() hasSentFollowRequest: boolean;
+  @Input() isAffiliated: boolean;
+  @Input() hasSentAffiliationRequest: boolean;
+  UserType = UserType;
+  profileUrl: string;
 
   constructor(
     private userService: UserService,
     private toastController: ToastController,
-    private actionSheetController: ActionSheetController,
     private socialSharing: SocialSharing
   ) {}
 
   ngOnInit() {
-    this.getFollowersFollowingAndAffiliatedUsers()
     this.profileUrl =
-      "http://localhost:4200/profile/shared/" + this.profile.userId
+      'http://localhost:4200/profile/shared/' + this.profile.userId;
   }
 
   follow() {
@@ -49,21 +45,21 @@ export class OverviewComponent implements OnInit {
         async (followRequest: FollowRequest) => {
           if (followRequest) {
             const toast = await this.toastController.create({
-              message: "Follow Request sent successfully.",
+              message: 'Follow Request sent successfully.',
               duration: 2000
-            })
-            toast.present()
+            });
+            toast.present();
           }
-          this.getFollowersFollowingAndAffiliatedUsers()
+          this.init.emit();
         },
         async (err: any) => {
           const toast = await this.toastController.create({
             message: err,
             duration: 2000
-          })
-          toast.present()
+          });
+          toast.present();
         }
-      )
+      );
   }
 
   unfollow() {
@@ -73,8 +69,8 @@ export class OverviewComponent implements OnInit {
         this.loggedInUser.userId.toString()
       )
       .subscribe(() => {
-        this.getFollowersFollowingAndAffiliatedUsers()
-      })
+        this.init.emit();
+      });
   }
 
   sendAffiliationRequest() {
@@ -84,75 +80,42 @@ export class OverviewComponent implements OnInit {
         async (affiliationRequest: AffiliationRequest) => {
           if (affiliationRequest) {
             const toast = await this.toastController.create({
-              message: "Affiliation Request sent successfully.",
+              message: 'Affiliation Request sent successfully.',
               duration: 2000
-            })
-            toast.present()
+            });
+            toast.present();
           }
-          this.getFollowersFollowingAndAffiliatedUsers()
+          this.init.emit();
         },
         async (err: any) => {
           const toast = await this.toastController.create({
             message: err,
             duration: 2000
-          })
-          toast.present()
+          });
+          toast.present();
         }
-      )
+      );
   }
 
   removeAffiliation() {
     this.userService
       .removeAffiliatedUser(this.loggedInUser.userId, this.profile.userId)
       .subscribe(() => {
-        this.getFollowersFollowingAndAffiliatedUsers()
-      })
+        this.init.emit();
+      });
   }
 
   isPublic() {
     return (
       this.profile.accountPrivacySetting == AccountPrivacySettingEnum.PUBLIC
-    )
-  }
-
-  getFollowersFollowingAndAffiliatedUsers() {
-    forkJoin([
-      this.userService.getFollowers(this.profile.userId),
-      this.userService.getFollowing(this.profile.userId),
-      this.userService.getFollowing(this.loggedInUser.userId),
-      this.userService.getFollowRequestMade(this.loggedInUser.userId),
-      this.userService.getAffiliatedUsers(this.profile.userId),
-      this.userService.getAffiliationRequestMade(this.loggedInUser.userId)
-    ]).subscribe((result) => {
-      this.profile = { ...this.profile, followers: result[0] }
-      this.profile = { ...this.profile, following: result[1] }
-      this.loggedInUser = { ...this.loggedInUser, following: result[2] }
-      this.isFollowing = this.loggedInUser.following
-        .map((user) => user.userId)
-        .includes(this.profile.userId)
-      this.hasSentFollowRequest = result[3]
-        .map((followRequestMade: FollowRequest) => {
-          return followRequestMade.to.userId
-        })
-        .includes(this.profile.userId)
-      this.profile = { ...this.profile, affiliatedUsers: result[4] }
-      this.isAffiliated = this.profile.affiliatedUsers
-        .map((user) => user.userId)
-        .includes(this.loggedInUser.userId)
-      this.hasSentAffiliationRequest = result[5]
-        .map((affiliationRequestMade: AffiliationRequest) => {
-          return affiliationRequestMade.to.userId
-        })
-        .includes(this.profile.userId)
-      console.log("overview" + this.profile)
-    })
+    );
   }
 
   sShare() {
     var options = {
-      message: "share this",
+      message: 'share this',
       url: this.profileUrl
-    }
-    this.socialSharing.shareWithOptions(options)
+    };
+    this.socialSharing.shareWithOptions(options);
   }
 }
