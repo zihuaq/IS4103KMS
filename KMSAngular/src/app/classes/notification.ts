b85b55913abe4b31dfa0d9f430f6344ad0d463b4
jeibeisoc <jiebei@comp.nus.edu.sg>
2020-11-09 11:15:46 +0800

    add notification class and service

---
+++ KMSAngular/src/app/classes/notification.ts
@@ -0,0 +1,22 @@
+import { User } from './user';
+
+export class Notification {
+    notificationId: number;
+    msg: string;
+    projectId: number;
+    groupId: number;
+    to: User;
+    hasRead: boolean;
+    date: Date;
+
+    constructor(notificationId?: number, msg?: string, projectId?: number, groupId?: number, to?: User, hasRead?: boolean, date?: Date) {
+        this.notificationId = notificationId;
+        this.msg = msg;
+        this.projectId = projectId;
+        this.groupId = groupId;
+        this.to = to;
+        this.hasRead = hasRead;
+        this.date = date;
+    }
+
+}
---
+++ KMSAngular/src/app/notification.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Notification } from './classes/notification';
+
+const httpOptions = {
+	headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  baseUrl: string = '/api/notification';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getNotification(userId: number): Observable<any> {
+    return this.httpClient.get<any>(this.baseUrl + "/getNotification/" + userId).pipe();
+  }
+
+  getNewNotification(userId: number): Observable<any> {
+    return this.httpClient.get<any>(this.baseUrl + "/getNewNotification/" + userId).pipe();
+  }
+
+  createNewNotification(newNotification: Notification, userId: number): Observable<any> {
+    let createNotificationReq = {
+      "newNotification": newNotification,
+      "userId": userId
+    }
+
+    return this.httpClient.put<any>(this.baseUrl + "/createNewNotification", createNotificationReq, httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  deleteNotification(notificationId: number) {
+    return this.httpClient.delete(this.baseUrl + "/deleteNotification/" + notificationId).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  readNotification(notificationId: number) {
+    return this.httpClient.post<any>(this.baseUrl + "/readNotification/" + notificationId, null).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage: string = '';
+    
+    if (error.error instanceof ErrorEvent) {
+        errorMessage = 'An unknown error has occurred: ' + error.error.message;
+    } else {
+        errorMessage = 'An HTTP error has occurred: ' + `HTTP ${error.status}: ${error.error.message}`;
+    }
+    return throwError(errorMessage)
+  }
+
+}
