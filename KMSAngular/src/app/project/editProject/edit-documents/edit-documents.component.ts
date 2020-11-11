86978e1aa580a54c8037ef8fe56d0ac9130c4677
jeibeisoc <jiebei@comp.nus.edu.sg>
2020-11-11 14:10:40 +0800

    add metadata for documents and display them

--- KMSAngular/src/app/project/editProject/edit-documents/edit-documents.component.ts
+++ KMSAngular/src/app/project/editProject/edit-documents/edit-documents.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as AWS from 'aws-sdk';
 import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/classes/user';
 import { SessionService } from 'src/app/session.service';
 import { Notification } from 'src/app/classes/notification';
 import { NotificationService } from 'src/app/notification.service';
@@ -28,15 +29,20 @@ export class EditDocumentsComponent implements OnInit {
   fileToUpload: File = null;
   fileToDelete;
   project: Project;
+  currentUser: User;
+  description: string;
+  docs: Document[];
 
   constructor(private activatedRouter: ActivatedRoute,
     private sessionService: SessionService,
     private notificationService: NotificationService,
     private projectService: ProjectService) { 
     this.files = [];
+    this.docs = [];
   }
 
   ngOnInit(): void {
+    this.currentUser = this.sessionService.getCurrentUser();
     this.projectId = parseInt(this.activatedRouter.snapshot.paramMap.get("projectId"));
     this.projectService.getProjectById(this.projectId).subscribe(
       response => {
@@ -50,18 +56,43 @@ export class EditDocumentsComponent implements OnInit {
   }
 
   async getObjects() {
+    this.docs = [];
     const { Contents } = await this.s3
      .listObjectsV2({ Bucket: this.bucket, Prefix: this.projectId.toString(), StartAfter: this.projectId.toString() + "/"})
      .promise();
      this.files = Contents;
+    
+    for (let i = 0; i < this.files.length; i++) {
+      let doc = new Document();
+      doc.key = this.files[i].Key;
+      doc.timeStamp = this.files[i].LastModified;
+      var par = {
+        Bucket: this.bucket, 
+        Key: this.files[i].Key
+       };
+      await this.s3.getObjectTagging(par, function(err, data) {
+        if (err) {
+          // error
+        }
+        else {
+          doc.author = data.TagSet[0].Value;
+          doc.description = data.TagSet[1].Value;
+          
+        }        
+      }).promise().then(
+        () => {
+          this.docs.push(doc);
+        }        
+      )      
+    }
     return Contents;
    }
   
 
-  viewFile(file) {
+  viewFile(key) {
     var params = {
       Bucket: this.bucket,
-      Key: file.Key,
+      Key: key,
     }
     console.log(params);
     let url = this.s3.getSignedUrl("getObject", params)
@@ -69,8 +100,8 @@ export class EditDocumentsComponent implements OnInit {
   }
 
   formatFileKey(file) {
-    let index = file.Key.indexOf("/");
-    return file.Key.substring(index + 1);
+    let index = file.indexOf("/");
+    return file.substring(index + 1);
   }
 
   handleFileInput(files: FileList) {
@@ -88,26 +119,39 @@ export class EditDocumentsComponent implements OnInit {
       });
       return;
     }
+
     const params = {
       Bucket: this.bucket,
       Key: this.projectId + "/" + this.fileToUpload.name,
-      Body: this.fileToUpload
+      Body: this.fileToUpload,
     }
 
-    this.s3.upload(params, function(err, data) {
+    this.s3.upload(params, 
+      {
+        tags: [
+          {
+            Key: 'author',
+            Value: this.currentUser.firstName + " " + this.currentUser.lastName,
+          },
+          {
+              Key: 'description',
+              Value: this.description,
+          },            
+        ],
+    },
+      function(err, data) {
       if (err) {
         console.log(err);
       }
     }).promise().then(
       () => {
-        let currentUser = this.sessionService.getCurrentUser();
         let newNotification = new Notification();
         newNotification.msg = "A new document has been uploaded to " + this.project.name;
         newNotification.projectId = this.projectId;
         newNotification.groupId = null;
         newNotification.projectTab = "document-tab";
         for (let member of this.project.projectMembers) {
-          if (member.userId != currentUser.userId) {
+          if (member.userId != this.currentUser.userId) {
             this.notificationService.createNewNotification(newNotification, member.userId).subscribe();
           }
         }
@@ -149,4 +193,18 @@ export class EditDocumentsComponent implements OnInit {
     );        
   }
 
+}
+
+export class Document {
+  key: string;
+  author: string;
+  description: string;
+  timeStamp: string;
+
+  constructor(key?: string, author?: string, description?: string, timeStamp?: string) {
+    this.key = key;
+    this.author = author;
+    this.description = description;
+    this.timeStamp = timeStamp;
+  }
 }
