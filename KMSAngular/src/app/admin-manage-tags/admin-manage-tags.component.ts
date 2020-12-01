bc3e7f8ebaad2b91c70be826a3ade829467d4c24
K-Cassie <khoocassie@ymail.com>
2020-12-01 19:36:14 +0800

    reorganise admin panel

---
+++ KMSAngular/src/app/admin-manage-tags/admin-manage-tags.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-manage-tags',
+  templateUrl: './admin-manage-tags.component.html',
+  styleUrls: ['./admin-manage-tags.component.css']
+})
+export class AdminManageTagsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
