db81a2f4e5b5f1000b8cd9326ee51e2615d29c25
K-Cassie <khoocassie@ymail.com>
2020-12-03 17:26:56 +0800

    added project matches rsp for ionic

--- KMSIonic/src/app/services/election.service.ts
+++ KMSIonic/src/app/services/election.service.ts
@@ -7,6 +7,7 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { Election } from '../classes/election';
+import { ElectionApplication } from '../classes/election-application';
 import { UtilityService } from './utility.service';
 
 const httpOptions = {
